4f91184da9b08cb3f5a9058a51c2c1cf0f8634e2
Pechanda <pechandav@gmail.com>
2020-07-14 18:25:40 -0700

    save

--- script.js
+++ script.js
@@ -9,4 +9,18 @@ $(document).ready(function() {
         nowHour= 13;
         nowHour=1;
     }
-}
+
+    let $dateHeading= $('#navbar-subtitle');
+    $dateHeading.text(now);
+
+    const saveIcon="./images/save-regular.svg";
+    let storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
+    if (test) {console.log(storedPlans);}
+    if (storedPlans !== null) {
+        planTextArr = storedPlans;
+        planTextArr= new Array(9);
+        planTextArr[4]="Buy more garlic";
+        if (test) {console.log("full array of plan text", planTextArr);}
+    }
+
+})
