85b2dbe06bf03d5615bd333f35d7142b43a275be
Pechanda <pechandav@gmail.com>
2020-07-14 17:44:24 -0700

    adding date and time using moment

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    const test = false;
+    const now=moment().format("MMMM Do YYYY");
+
+    let nowHour24 = moment().format('H');
+    let nowHour12= moment().format("h");
+
+    if (test) {
+        nowHour= 13;
+        nowHour=1;
+    }
+}
